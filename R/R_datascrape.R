5845dfc2ef7fc770bc5630e61bb2d6633b538393
Alfred Assal <alfredassal@hotmail.ca>
2022-12-06 18:52:17 -0500

    Commit de la premiere base de donnée sur le CO2

--- R/R_datascrape.R
+++ R/R_datascrape.R
@@ -1,3 +1,7 @@
-library(rvest)
+# ajout du dataset
+library(readr)
+library(RCurl)
+currentDate <- Sys.Date()
+csvFileName <- paste("data/raw/CO2",currentDate,".csv",sep = "_")
+download.file("https://nyc3.digitaloceanspaces.com/owid-public/data/co2/owid-co2-data.csv",destfile = csvFileName)
 
-datCO2 <- write.csv("https://nyc3.digitaloceanspaces.com/owid-public/data/co2/owid-co2-data.csv","data/raw")
