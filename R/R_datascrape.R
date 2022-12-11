085634f7fee8e6209bd978392e611f126776b82a
Alfred Assal <alfredassal@hotmail.ca>
2022-12-11 11:56:12 -0500

    Commit de la base du travail, données et début de l'observation des données

--- R/R_datascrape.R
+++ R/R_datascrape.R
@@ -1,7 +1,6 @@
 # ajout du dataset
-library(readr)
-library(RCurl)
+
 currentDate <- Sys.Date()
-csvFileName <- paste("data/raw/CO2",currentDate,".csv",sep = "_")
-download.file("https://nyc3.digitaloceanspaces.com/owid-public/data/co2/owid-co2-data.csv",destfile = csvFileName)
+csvFileName <- paste("data/raw/energie",currentDate,".csv",sep = "_")
+download.file("https://nyc3.digitaloceanspaces.com/owid-public/data/energy/owid-energy-data.csv",destfile = csvFileName)
 
